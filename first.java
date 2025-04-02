7ac2a1491e80572e0a43ac97d938ac2f1b7c5f71
Hemanth <you@example.com>
2025-04-02 20:38:35 +0530

    2nd cmt

--- first.java
+++ first.java
@@ -1,5 +1,8 @@
 public class first {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+          System.out.println("Hello, babbu!");
+            System.out.println("Hello, devva!");
+              System.out.println("Hello, kavaya!");
     }
 }
